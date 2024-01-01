38471110f2e9133033bdcc2ac1dc7bc9b5a0aa0b
David Chubabriya <david.chubabriya@gmail.com>
2024-01-01 22:49:52 +0000

    Update spa routes

--- spa/src/router/index.ts
+++ spa/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/JobsView.vue')
+      component: () => import('../views/ImagesView.vue')
     },
     {
       path: '/sign-in',
